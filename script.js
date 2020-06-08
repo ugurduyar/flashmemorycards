a93ca1d243e7bcff34654b0f108e146abf9204ad
ugurduyar <ismailugurduyar@gmail.com>
2020-06-08 23:42:03 +0300

    flip cards added

--- script.js
+++ script.js
@@ -9,3 +9,67 @@ const answerEl = document.getElementById("answer");
 const addCardBtn = document.getElementById("add-card");
 const clearBtn = document.getElementById("clear");
 const addContainer = document.getElementById("add-container");
+
+// Check current card
+
+let currentActiveCard = 0;
+
+// Store DOM cards
+const cardsEl = [];
+
+// Store card data
+const cardsData = [
+  {
+    question: "What must a variable begin with?",
+    answer: "A letter, $ or _",
+  },
+  {
+    question: "What is a variable?",
+    answer: "Container for a piece of data",
+  },
+  {
+    question: "Example of Case Sensitive Variable",
+    answer: "thisIsAVariable",
+  },
+];
+
+// Create all cards
+function createCards() {
+  cardsData.forEach((data, index) => createCard(data, index));
+}
+
+// Create a single card
+function createCard(data, index) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  if (index === 0) {
+    card.classList.add("active");
+  }
+
+  card.innerHTML = `
+    <div class="inner-card">
+          <div class="inner-card-front">
+            <p>${data.question}</p>
+          </div>
+          <div class="inner-card-back">
+            <p>${data.answer} </p>
+          </div>
+        </div>
+    
+    `;
+
+  card.addEventListener("click", () => card.classList.toggle("show-answer"));
+
+  // Add to dom cards
+  cardsEl.push(card);
+  cardsContainer.appendChild(card);
+
+  updateCurrentText();
+}
+
+function updateCurrentText() {
+    currentEl.innerText = `${}`
+}
+
+createCards();
