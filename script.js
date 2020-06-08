07033db3651e5ccbef098897db723fced8f4b6a5
ugurduyar <ismailugurduyar@gmail.com>
2020-06-09 00:05:06 +0300

    clear cards button sorted

--- script.js
+++ script.js
@@ -140,3 +140,10 @@ addCardBtn.addEventListener("click", () => {
     setCardsData(cardsData);
   }
 });
+
+// Clear cards button
+clearBtn.addEventListener("click", () => {
+  localStorage.clear();
+  cardsContainer.innerHTML = "";
+  window.location.reload();
+});
