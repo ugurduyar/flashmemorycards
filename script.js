3f968dda6386a523cb0178a9e520cddcbf890311
ugurduyar <ismailugurduyar@gmail.com>
2020-06-08 23:50:26 +0300

    go back added

--- script.js
+++ script.js
@@ -84,4 +84,20 @@ nextBtn.addEventListener("click", () => {
   }
 
   cardsEl[currentActiveCard].className = "card active";
+
+  updateCurrentText();
+});
+
+prevBtn.addEventListener("click", () => {
+  cardsEl[currentActiveCard].className = "card right";
+
+  currentActiveCard = currentActiveCard - 1;
+
+  if (currentActiveCard < 0) {
+    currentActiveCard = 0;
+  }
+
+  cardsEl[currentActiveCard].className = "card active";
+
+  updateCurrentText();
 });
